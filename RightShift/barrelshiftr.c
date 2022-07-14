64a77ae3d3964c4981e0985352c818668a08a86f
DanielFlyer <dflyer@princeton.edu>
2022-07-14 15:55:14 -0400

    ;

---
+++ RightShift/barrelshiftr.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stddef.h>
+
+unsigned int shiftr_c(unsigned int alu_a_i, unsigned int alu_b_i, char opcode){
+
+    unsigned int result_r, shift_left_1_r, shift_left_2_r, shift_left_4_r, shift_left_8_r;
+
+    opcode = opcode & 0xf;
+    if(opcode == 0x3 && ){
+        result_r = (int)alu_a_i >> (int)alu_b_i;
+    }else{
+        if ((alu_b_i & 0x1) != 0)
+                shift_left_1_r = alu_a_i << 1;
+            else
+                shift_left_1_r = alu_a_i;
+
+            if ((alu_b_i & 0x2) != 0)
+                shift_left_2_r = shift_left_1_r << 2;
+                
+            else
+                shift_left_2_r = shift_left_1_r;
+
+            if ((alu_b_i & 0x4) != 0)
+                shift_left_4_r = shift_left_2_r << 4;
+            else
+                shift_left_4_r = shift_left_2_r;
+
+            if ((alu_b_i & 0x8) != 0)
+                shift_left_8_r = shift_left_4_r << 8;
+            else
+                shift_left_8_r = shift_left_4_r;
+
+            if ((alu_b_i & 0x10) != 0)
+                result_r = shift_left_8_r << 16;
+            else
+                result_r = shift_left_8_r;
+    }
+
+    return result_r;
+}
