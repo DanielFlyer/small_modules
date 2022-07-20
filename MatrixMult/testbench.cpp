3f6255d94924cd8e4548cff5be5380b2c47fd8a2
DanielFlyer <dflyer@princeton.edu>
2022-07-20 15:48:33 -0400

    basic bench

---
+++ MatrixMult/testbench.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <iostream>
+#include <time.h>
+#include "verilated.h"
+#include "./obj_dir/Vtb.h"
+#include "matrixmult.c"
+
+int main(int argc, char **argv) {
+    //Initialize Verilators variables
+	Verilated::commandArgs(argc, argv);
+    //Create an instance of our module under test
+	Vtb *tb = new Vtb;
+    tb->eval();
